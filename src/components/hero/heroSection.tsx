f20b3eae3cd691d5bc1f073aed05c51aef7386ce
BeRe197 <benjamin@reiser-family.de>
2025-02-01 17:19:55 +0100

    add animation add gesture support

--- src/components/hero/heroSection.tsx
+++ src/components/hero/heroSection.tsx
@@ -11,6 +11,7 @@ import {
     alpha,
 } from '@mui/material';
 import {KeyboardArrowLeft, KeyboardArrowRight} from '@mui/icons-material';
+import {AnimatePresence, motion} from "framer-motion";
 // components
 import IconButton from "@/components/button/iconButton";
 import TypingText from "@/components/animation/typingText";
@@ -39,13 +40,9 @@ const HeroSection = ({heroPages}: { heroPages: HeroPage[] }) => {
     };
 
     useEffect(() => {
-        // Auto play - rotate every X seconds
+        // Autoplay - rotate every X seconds
         const timeout = setTimeout(() => {
-            if (activeStep === maxSteps - 1) {
-                setActiveStep(0);
-            } else {
-                setActiveStep((prevStep) => prevStep + 1);
-            }
+            setActiveStep((prevStep) => (prevStep + 1) % maxSteps);
         }, 6000);
         return () => clearTimeout(timeout);
     }, [activeStep, maxSteps]);
@@ -53,147 +50,165 @@ const HeroSection = ({heroPages}: { heroPages: HeroPage[] }) => {
     const currentPage = heroPages[activeStep];
 
     return (
-        <Box sx={{position: 'relative', width: '100%', height: '60vh'}}>
-            {/* Background Image */}
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    backgroundImage: `url(${currentPage.backgroundImage})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundAttachment: "fixed",
-                    backgroundRepeat: "no-repeat",
-                    '&::after': {
-                        content: '""',
+        <AnimatePresence mode="wait">
+            <Box sx={{position: 'relative', width: '100%', height: '60vh'}}
+                 component={motion.div}
+                 drag="x"
+                 dragConstraints={{left: 0, right: 0}}
+                 onDragEnd={(event, info) => {
+                     if (info.offset.x < -100) {
+                         setActiveStep((prevStep) => (prevStep + 1) % maxSteps);
+                     } else if (info.offset.x > 100) {
+                         setActiveStep((prevStep) => (prevStep - 1 + maxSteps) % maxSteps);
+                     }
+                 }}>
+                {/* Background Image */}
+                <Box
+                    sx={{
                         position: 'absolute',
                         top: 0,
                         left: 0,
                         right: 0,
                         bottom: 0,
-                        backgroundColor: 'rgba(0, 0, 0, 0.25)', // Dark overlay
-                    },
-                }}
-            />
-
-            {/* Content */}
-            <Container
-                sx={{
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    position: 'relative',
-                    zIndex: 1,
-                    color: 'white',
-                    '& .MuiContainer-root': {
-                        padding: 'inherit',
-                    },
-                    ...container
-                }}
-            >
-                <TypingText words={[currentPage.title]}/>
-
-                <Typography
-                    variant="h5"
-                    color={"white"}
-                    sx={{
-                        mb: 4,
-                        animation: 'fadeIn 0.5s ease-in 0.2s',
-                    }}
-                >
-                    {currentPage.subtitle}
-                </Typography>
-
-                <Button
-                    variant="contained"
-                    size="large"
-                    href={currentPage.buttonLink}
-                    sx={btnAnimation}
-                >
-                    {currentPage.buttonText}
-                </Button>
-            </Container>
-
-            {/* Pagination Dots */}
-            <Paper
-                square
-                elevation={0}
-                sx={{
-                    position: 'absolute',
-                    bottom: 32,
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    backgroundColor: 'transparent',
-                    zIndex: 1,
-                }}
-            >
-                <MobileStepper
-                    variant="dots"
-                    steps={maxSteps}
-                    position="static"
-                    activeStep={activeStep}
-                    sx={{
-                        backgroundColor: 'transparent',
-                        '& .MuiMobileStepper-dot': {
-                            backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.3),
-                        },
-                        '& .MuiMobileStepper-dotActive': {
-                            backgroundColor: 'primary.main',
-                        },
-                        '& .Mui-disabled': {
-                            color: (theme) => alpha(theme.palette.primary.main, 0.3),
+                        backgroundImage: `url(${currentPage.backgroundImage})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundAttachment: "fixed",
+                        backgroundRepeat: "no-repeat",
+                        '&::after': {
+                            content: '""',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            backgroundColor: 'rgba(0, 0, 0, 0.25)', // Dark overlay
                         },
                     }}
-                    nextButton={
-                        <IconButton
-                            size="small"
-                            onClick={handleNext}
-                            disabled={activeStep === maxSteps - 1}
-                            color={"primary"}
-                        >
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowLeft/>
-                            ) : (
-                                <KeyboardArrowRight/>
-                            )}
-                        </IconButton>
-                    }
-                    backButton={
-                        <IconButton
-                            size="small"
-                            onClick={handleBack}
-                            disabled={activeStep === 0}
-                            color={"primary"}
-                        >
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowRight/>
-                            ) : (
-                                <KeyboardArrowLeft/>
-                            )}
-                        </IconButton>
-                    }
+                    component={motion.div}
+                    key={activeStep}
+                    initial={{opacity: 0, x: 20}}
+                    animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: -20}}
+                    transition={{duration: 0.8, ease: "easeInOut"}}
                 />
-            </Paper>
 
-            {/* CSS Animation */}
-            <style jsx global>{`
-                @keyframes fadeIn {
-                    from {
-                        opacity: 0;
-                        transform: translateY(20px);
+                {/* Content */}
+                <Container
+                    sx={{
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        position: 'relative',
+                        zIndex: 1,
+                        color: 'white',
+                        '& .MuiContainer-root': {
+                            padding: 'inherit',
+                        },
+                        ...container
+                    }}
+                >
+                    <TypingText words={[currentPage.title]}/>
+
+                    <Typography
+                        variant="h5"
+                        color={"white"}
+                        sx={{
+                            mb: 4,
+                            animation: 'fadeIn 0.5s ease-in 0.2s',
+                        }}
+                    >
+                        {currentPage.subtitle}
+                    </Typography>
+
+                    <Button
+                        variant="contained"
+                        size="large"
+                        href={currentPage.buttonLink}
+                        sx={btnAnimation}
+                    >
+                        {currentPage.buttonText}
+                    </Button>
+                </Container>
+
+                {/* Pagination Dots */}
+                <Paper
+                    square
+                    elevation={0}
+                    sx={{
+                        position: 'absolute',
+                        bottom: 32,
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        backgroundColor: 'transparent',
+                        zIndex: 1,
+                    }}
+                >
+                    <MobileStepper
+                        variant="dots"
+                        steps={maxSteps}
+                        position="static"
+                        activeStep={activeStep}
+                        sx={{
+                            backgroundColor: 'transparent',
+                            '& .MuiMobileStepper-dot': {
+                                backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.3),
+                            },
+                            '& .MuiMobileStepper-dotActive': {
+                                backgroundColor: 'primary.main',
+                            },
+                            '& .Mui-disabled': {
+                                color: (theme) => alpha(theme.palette.primary.main, 0.3),
+                            },
+                        }}
+                        nextButton={
+                            <IconButton
+                                size="small"
+                                onClick={handleNext}
+                                disabled={activeStep === maxSteps - 1}
+                                color={"primary"}
+                            >
+                                {theme.direction === 'rtl' ? (
+                                    <KeyboardArrowLeft/>
+                                ) : (
+                                    <KeyboardArrowRight/>
+                                )}
+                            </IconButton>
+                        }
+                        backButton={
+                            <IconButton
+                                size="small"
+                                onClick={handleBack}
+                                disabled={activeStep === 0}
+                                color={"primary"}
+                            >
+                                {theme.direction === 'rtl' ? (
+                                    <KeyboardArrowRight/>
+                                ) : (
+                                    <KeyboardArrowLeft/>
+                                )}
+                            </IconButton>
+                        }
+                    />
+                </Paper>
+
+                {/* CSS Animation */}
+                <style jsx global>{`
+                    @keyframes fadeIn {
+                        from {
+                            opacity: 0;
+                            transform: translateY(20px);
+                        }
+                        to {
+                            opacity: 1;
+                            transform: translateY(0);
+                        }
                     }
-                    to {
-                        opacity: 1;
-                        transform: translateY(0);
-                    }
-                }
-            `}</style>
-        </Box>
+                `}</style>
+            </Box>
+        </AnimatePresence>
     );
 };
 
