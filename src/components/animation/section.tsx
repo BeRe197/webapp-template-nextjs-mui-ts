3bddd1400669e1f5cb0f469ee58fca0d90af3d64
BeRe197 <benjamin@reiser-family.de>
2025-02-01 11:22:47 +0100

    make once configurable

--- src/components/animation/section.tsx
+++ src/components/animation/section.tsx
@@ -2,13 +2,19 @@
 import {ReactNode} from "react";
 import {motion} from "framer-motion";
 
-const Section = ({children, style}: { children: ReactNode, style?: object }) => {
+interface props {
+    children: ReactNode,
+    style?: object,
+    once?: boolean
+}
+
+const Section = ({children, style, once = false}: props) => {
     return (
         <motion.div
             initial={{opacity: 0, y: 50}}
             whileInView={{opacity: 1, y: 0}}
             transition={{duration: 1, ease: "easeOut"}}
-            viewport={{once: true}}
+            viewport={{once: once}}
             className="my-20 p-10 bg-gray-100 rounded-lg"
             style={style}
         >
