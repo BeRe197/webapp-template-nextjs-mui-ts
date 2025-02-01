ffb0edee306bd7129239cc2e41c96363ed60fdc9
BeRe197 <benjamin@reiser-family.de>
2025-02-01 11:29:06 +0100

    add btn animation

--- src/assets/jss/global.style.ts
+++ src/assets/jss/global.style.ts
@@ -25,6 +25,16 @@ const container = {
 const btnAnimation = {
     width: 'fit-content',
     animation: 'fadeIn 0.5s ease-in 0.4s',
+    transition: "all .5s ease-in-out",
+    "&:hover": {
+        /*  Making button bigger on hover  */
+        transform: "scale(1.08) perspective(1px)"
+    },
+    "&:active": {
+        transform: "translate(0px, 5px)",
+        WebkitTransform: "translate(0px, 5px)",
+        boxShadow: "0px 1px 0px 0px"
+    }
 }
 
 export {
